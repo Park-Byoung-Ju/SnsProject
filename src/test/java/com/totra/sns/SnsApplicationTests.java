f16a8432f8ed23460779cd9f15ca7d5530aca426
t9864 노트북 <qudwn9898@gmail.com>
2024-11-29 17:28:38 +0900

    박병주  - spring sns project 생성

---
+++ src/test/java/com/totra/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totra.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
